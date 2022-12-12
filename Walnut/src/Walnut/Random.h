6b4df7034c5476235d27f2fc36e68a8b83990036
PSHamke <hamkelektrik@gmail.com>
2022-12-12 03:26:15 +0300

    Remove unused glm functions for now

--- Walnut/src/Walnut/Random.h
+++ Walnut/src/Walnut/Random.h
@@ -2,7 +2,6 @@
 
 #include <random>
 
-#include <glm/glm.hpp>
 
 namespace Walnut {
 
@@ -29,20 +28,6 @@ namespace Walnut {
 			return (float)s_Distribution(s_RandomEngine) / (float)std::numeric_limits<uint32_t>::max();
 		}
 
-		static glm::vec3 Vec3()
-		{
-			return glm::vec3(Float(), Float(), Float());
-		}
-
-		static glm::vec3 Vec3(float min, float max)
-		{
-			return glm::vec3(Float() * (max - min) + min, Float() * (max - min) + min, Float() * (max - min) + min);
-		}
-
-		static glm::vec3 InUnitSphere()
-		{
-			return glm::normalize(Vec3(-1.0f, 1.0f));
-		}
 	private:
 		static std::mt19937 s_RandomEngine;
 		static std::uniform_int_distribution<std::mt19937::result_type> s_Distribution;
